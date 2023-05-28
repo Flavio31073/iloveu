40e72541952e1c0d4f8c407d20cac8e0db4e279b
Flavio31073 <102006511+Flavio31073@users.noreply.github.com>
2023-05-28 14:09:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("sim").addEventListener("click", function() {
+  alert("Muito obrigado! Te amo muito!");
+});
+
+document.getElementById("nao").addEventListener("click", function() {
+  alert("Estou triste por isso...");
+});
